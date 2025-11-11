0f78aa3d3b5fd9a076bfdb927f846cc09f13e0b5
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-11-11 14:53:21 +0100

    fix: double initLog() call

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,16 @@ const std = @import("std");
 const kom = @import("root.zig");
 const game = kom.game;
 
-const prefix_log: []const u8 = "[log]:";
-
 var running: std.atomic.Value(bool) = .init(false);
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
-    const alloc = gpa.allocator();
-    // const alloc = std.heap.page_allocator;
-    const props = try kom.findOrCreateProperties(alloc);
 
+    const alloc = gpa.allocator();
+    const props = try kom.findOrCreateProperties(alloc);
     const addr = try std.net.Address.parseIp4(props.host, 0);
 
     try game.startServer(props, addr, &running, update);
-    try game.initLog(prefix_log);
-    try game.initInput();
 }
 
 pub fn update() void {
